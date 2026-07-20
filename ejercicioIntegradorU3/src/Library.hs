ee039997cd945574324cec1b447610c6f19d9978
br4pz <brunapreal@gmail.com>
2026-07-20 16:27:14 -0300

    resolucion ejercicio

--- ejercicioIntegradorU3/src/Library.hs
+++ ejercicioIntegradorU3/src/Library.hs
@@ -1,5 +1,42 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+data Alumno = Alumno{
+    nombre :: String,
+    fechaNacimiento :: String,
+    legajo :: Number,
+    materias :: [String],
+    criterio :: Criterio
+}deriving(Show)
+
+type Criterio = Parcial -> Bool
+
+data Parcial = Parcial{
+    cantidadPreguntas :: Number,
+    materia :: String
+}
+
+cabulero :: Criterio
+cabulero = odd.length.materia
+
+hijoDelRigor :: Number -> Criterio
+hijoDelRigor n = (n<).cantidadPreguntas
+
+estudioso :: Criterio
+estudioso _ = True
+
+nico = Alumno{
+    nombre ="Nico",
+    fechaNacimiento = "11 enero 2003",
+    legajo = 1231234,
+    materias = ["Fisica", "Pdep"],
+    criterio = estudioso
+}
+
+cambiarCriterio :: Criterio -> Alumno -> Alumno
+cambiarCriterio nuevoCriterio alumno = alumno {criterio = nuevoCriterio}
+
+estudiaParaParcial :: Parcial -> Alumno -> Bool
+estudiaParaParcial parcial alumno = criterio alumno parcial
+
+parcialPDP = Parcial 3 "PDP" 
